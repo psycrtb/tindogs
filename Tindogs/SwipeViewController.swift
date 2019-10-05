086abe30c8a5aa7d7d656362399a82e118ad2277
Tim Belfield <timothy.belfield@ready2order.com>
2019-10-05 12:41:00 +0200

    yes

---
+++ Tindogs/SwipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SwipeViewController.swift
+//  Tindogs
+//
+//  Created by Tim Belfield on 05.10.19.
+//  Copyright © 2019 Tindogs. All rights reserved.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+    
+    @IBOutlet weak var bioTextView: UITextView!
+    @IBOutlet weak var bioTextConstraint: NSLayoutConstraint!
+    
+
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Populate text here
+        // bioTextView
+        
+//        let size = CGSize(
+        
+        
+    }
+}
